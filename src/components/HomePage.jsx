34f986c74314c2ae2ce4fccc1870527a76d014f5
Jallal ☁ <jhazzat@gmail.com>
2023-12-22 17:48:35 +0000

    [Jallal] initial work

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class HomePage extends React.Component {
+
+    render() {
+        return (
+<>
+<h1>Hello World : let's start with this shit</h1>
+</>
+);
+}
+}
+export default HomePage;
