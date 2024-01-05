e86f82ffcab520d5fe280a7024228cd18fcce206
Jallal ☁ <jhazzat@gmail.com>
2024-01-05 04:43:47 +0000

    [Jallal] added multiple pages to this site

---
+++ src/components/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../header/Header.jsx";
+import Footer from "../footer/Footer.js";
+
+function AboutPage(){
+ 
+    
+return (
+    <div>
+        <Header />
+      <div>About This page</div>
+        <Footer />
+    </div>
+)
+}
+export default AboutPage
