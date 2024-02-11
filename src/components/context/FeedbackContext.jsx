f1b00cb26f77e8a2341d97d8f84be8ce8c500329
Jallal ☁ <jhazzat@gmail.com>
2024-02-11 20:20:34 +0000

    [Jallal] push latest changes

--- src/components/context/FeedbackContext.jsx
+++ src/components/context/FeedbackContext.jsx
@@ -42,20 +42,21 @@ const [feedBackEdit, setFeedBackEdit] = useState({
     }
 
     const updateFeedback = (id, updatedItem) => {
+        
         setFeedback(feedback.map((item) => (item.id === id ? {...item, ...updatedItem } : item )))
     }
 
 
     const deleteFeedback =(id) => {
+
         console.log('app',id)
-    
         if(window.confirm('Are you sure you want to delete?')){
-    
           setFeedback(feedback.filter((item) => item.id!==id))
         }
       }
 
       const addFeedback = (newFeedback) => {
+
         newFeedback.id = uuidv4()
         setFeedback([newFeedback, ...feedback])
       }
